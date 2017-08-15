56ea171c1a0b30c15e4f0aced1a17dfc54d522fb
Nolan Kamitaki <nkamitaki@gmail.com>
2017-08-15 00:11:29 -0400

    Create cgminer.rb

---
+++ cgminer.rb
@@ -0,0 +1,47 @@
+require 'formula'
+
+class Cgminer < Formula
+  homepage 'https://github.com/ckolivas/cgminer'
+  head 'https://github.com/ckolivas/cgminer.git', :branch => 'master'
+  url 'https://github.com/ckolivas/cgminer/archive/fc33786a4b8699c95db382ab6fd4969a527cd5dd.tar.gz'
+  sha256 '6a93d065551bde115905037c71398b57cff0efa569bd8e229a20e3d36e598be1'
+  version "4.10.0"
+
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
+  depends_on 'libtool' => :build
+  depends_on 'pkg-config' => :build
+  depends_on 'coreutils' => :build
+  depends_on 'curl'
+  depends_on 'libusb'
+  depends_on 'ncurses'
+  depends_on 'jansson'
+
+  def install
+    system "autoreconf -fvi"
+    system "./autogen.sh", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "PKG_CONFIG_PATH=#{HOMEBREW_PREFIX}/opt/curl/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/jansson/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/libusb/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/ncurses/lib/pkgconfig",
+                          "--enable-bflsc",
+                          "--enable-bitforce",
+                          "--enable-icarus",
+                          "--enable-modminer",
+                          "--enable-avalon",
+                          "--disable-adl",
+                          "--enable-opencl",
+                          "--enable-bitfury",
+                          "--enable-hashfast",
+                          "--enable-klondike",
+                          "--enable-drillbit",
+                          "--enable-avalon2",
+                          "--enable-blockerupter",
+                          "--enable-cointerra",
+                          "--enable-hashratio",
+                          "--with-system-jansson"
+    system "make", "install"
+  end
+
+  test do
+    system "cgminer"
+  end
+end
